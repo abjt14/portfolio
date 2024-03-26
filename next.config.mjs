c4b444d35644efb181d16f4f51c0b1b8d4f59c3c
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-03-26 15:10:29 -0700

    updatiing cofig with proper header values

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
+// https://nextjs.org/docs/app/api-reference/next-config-js/headers
+const ContentSecurityPolicy = `
+  default-src 'self' vercel.live;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live va.vercel-scripts.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'self' https://*.abjt.dev;
+  connect-src *;
+  font-src 'self';
+`;
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
 
 export default nextConfig;
