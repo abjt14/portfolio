84453716402039828ff9f28684aa316c227261e8
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-03-30 07:15:04 -0700

    added option to only listen for isMobile

--- src/hooks/useWindowBreakpoints.js
+++ src/hooks/useWindowBreakpoints.js
@@ -2,33 +2,41 @@
 
 import React from "react";
 
-export default function useWindowBreakpoints() {
+export default function useWindowBreakpoints({ isMobile = false }) {
   const [breakpoint, setBreakpoint] = React.useState(false);
 
   React.useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 640 && breakpoint !== "xs") {
-        setBreakpoint("xs");
-      } else if (
-        window.innerWidth >= 640 &&
-        window.innerWidth < 768 &&
-        breakpoint !== "sm"
-      ) {
-        setBreakpoint("sm");
-      } else if (
-        window.innerWidth >= 768 &&
-        window.innerWidth < 1024 &&
-        breakpoint !== "md"
-      ) {
-        setBreakpoint("md");
-      } else if (
-        window.innerWidth >= 1024 &&
-        window.innerWidth < 1280 &&
-        breakpoint !== "lg"
-      ) {
-        setBreakpoint("lg");
-      } else if (window.innerWidth >= 1280 && breakpoint !== "xl") {
-        setBreakpoint("xl");
+      if (!isMobile) {
+        if (window.innerWidth < 640 && breakpoint !== "xs") {
+          setBreakpoint("xs");
+        } else if (
+          window.innerWidth >= 640 &&
+          window.innerWidth < 768 &&
+          breakpoint !== "sm"
+        ) {
+          setBreakpoint("sm");
+        } else if (
+          window.innerWidth >= 768 &&
+          window.innerWidth < 1024 &&
+          breakpoint !== "md"
+        ) {
+          setBreakpoint("md");
+        } else if (
+          window.innerWidth >= 1024 &&
+          window.innerWidth < 1280 &&
+          breakpoint !== "lg"
+        ) {
+          setBreakpoint("lg");
+        } else if (window.innerWidth >= 1280 && breakpoint !== "xl") {
+          setBreakpoint("xl");
+        }
+      } else {
+        if (window.innerWidth < 640 && breakpoint !== "xs") {
+          setBreakpoint("xs");
+        } else if (window.innerWidth >= 640 && breakpoint !== "sm") {
+          setBreakpoint("sm");
+        }
       }
     };
 
@@ -36,7 +44,7 @@ export default function useWindowBreakpoints() {
     handleResize();
 
     return () => window.removeEventListener("resize", handleResize);
-  }, [breakpoint]);
+  }, [breakpoint, isMobile]);
 
   return breakpoint;
 }
