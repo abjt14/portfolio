5315aee29717edc99fed316ba56c52b432090e48
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-06-29 10:43:24 -0700

    added option to use placeholder poster and pass classnames to both parent and child containers

--- src/components/CustomMDX/components/MDXVideo.js
+++ src/components/CustomMDX/components/MDXVideo.js
@@ -1,10 +1,21 @@
+import clsx from "clsx";
+
 export default function MDXVideo(props) {
+  const aspectRatio = `${props.width} / ${props.height}`;
   return (
-    <div className="w-full h-auto flex flex-col justify-center items-center my-2 sm:my-6">
+    <div
+      className={clsx(
+        "w-full h-auto flex flex-col justify-center items-center my-2 sm:my-6",
+        props.parentClassName ? props.parentClassName : ""
+      )}
+    >
       <div
-        className="w-full h-auto rounded-xl border border-neutral-300 dark:border-neutral-850 relative overflow-hidden"
+        className={clsx(
+          "w-full h-auto rounded-xl border border-neutral-300 dark:border-neutral-850 relative overflow-hidden",
+          props.className ? props.className : ""
+        )}
         style={{
-          aspectRatio: `${props.width} / ${props.height}`,
+          aspectRatio,
         }}
       >
         <video
@@ -16,12 +27,21 @@ export default function MDXVideo(props) {
           width={props.width}
           height={props.height}
           type="video/mp4"
-          className="w-full h-auto"
+          className="w-full h-auto z-20 relative"
           loading="lazy"
           style={{
-            aspectRatio: `${props.width} / ${props.height}`,
+            aspectRatio,
           }}
         />
+        {props.poster && (
+          <div
+            className="absolute top-0 left-0 h-full w-full bg-no-repeat bg-cover blur-xl z-10"
+            style={{
+              backgroundImage: `url(${props.poster})`,
+              aspectRatio,
+            }}
+          />
+        )}
       </div>
       <p className="text-xs text-neutral-500 text-center text-balance pt-2 px-2">
         {props.alt}
