88100aa06d8d2cd4ec4f4a20a3e0aeeaaff763fa
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-07-01 16:20:03 -0700

    moved file into component folder

--- src/app/lab/[slug]/components/ProjectLink.js
+++ src/app/lab/[slug]/components/ProjectLink.js
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import clsx from "clsx";
 
-export default function ProjectLink({ href, name }) {
+export default function ProjectLink({ href, name, type }) {
   return (
     <Link
       href={href}
       target="_blank"
-      className=" bg-neutral-200 dark:bg-neutral-800 p-px rounded-full relative shadow-sm shadow-neutral-400 dark:shadow-black group outline-none focus-visible:ring-1 ring-neutral-950 dark:ring-neutral-50"
+      className="h-[calc(100%-2px)] bg-neutral-200 dark:bg-neutral-800 p-px rounded-full relative shadow-sm shadow-neutral-400 dark:shadow-black group outline-none focus-visible:ring-1 ring-neutral-950 dark:ring-neutral-50"
       aria-label={`visit ${name}`}
     >
       <div
         className={clsx(
-          "w-full h-full flex justify-center items-center gap-0 py-3 pl-4 pr-3 rounded-full relative z-20",
+          "w-full h-full flex justify-center items-center gap-1 py-3 pl-4 pr-3 rounded-full relative z-20",
           "bg-gradient-to-tl from-neutral-50 dark:from-neutral-925 via-neutral-200 dark:via-neutral-900 to-neutral-50 dark:to-neutral-925",
           "text-neutral-500 dark:text-neutral-400 group-hover:text-neutral-950 dark:group-hover:text-neutral-50 transition-colors duration-150"
         )}
@@ -24,22 +24,23 @@ export default function ProjectLink({ href, name }) {
             "transition-[text-shadow] duration-150"
           )}
         >
-          Visit
+          {type == "production" ? "Production" : "CodePen"}
         </span>
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          color="currentColor"
           className={clsx(
-            "size-4 transition-[filter] duration-150",
+            "size-4 transition-[filter] duration-150 -rotate-45 stroke-2",
             "drop-shadow-none group-hover:[filter:drop-shadow(0px_0px_4px_rgba(10,_10,_10,_.5))] dark:group-hover:[filter:drop-shadow(0px_0px_4px_rgba(250,250,250,1))]"
           )}
         >
           <path
-            fillRule="evenodd"
-            d="M4.22 11.78a.75.75 0 0 1 0-1.06L9.44 5.5H5.75a.75.75 0 0 1 0-1.5h5.5a.75.75 0 0 1 .75.75v5.5a.75.75 0 0 1-1.5 0V6.56l-5.22 5.22a.75.75 0 0 1-1.06 0Z"
-            clipRule="evenodd"
-          />
+            d="M6 12h12.5m0 0l-6-6m6 6l-6 6"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
         </svg>
       </div>
       <div className="absolute top-0 left-0 h-full w-full rounded-full bg-gradient-to-br from-transparent from-30% via-neutral-300 dark:via-neutral-750 to-60% to-transparent z-10" />
